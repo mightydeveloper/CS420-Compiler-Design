d57a764dc7d5f58c7e56b09389a71bdf8c5923a3
Young Seok Tony Kim <mightydeveloper@gmail.com>
2016-12-14 04:33:38 +0900

    partially implemented expr and special functions

--- generator.py
+++ generator.py
@@ -16,6 +16,16 @@ import ErrorCollector
 # generator = TMachineCodeGenerator()
 # areaInstr = Instruction(Instruction.AREA, "MEM")
 # generator.addInstruction(areaInstr)
+
+def register(num):
+    return "R("+num+")"
+
+def getFromAddress(location):
+    return location + "@"
+
+def getFromRegister(num):
+    return "R("+num+")@"
+
 class Instruction(object):
     AREA = "AREA"
     MOVE = "MOVE"
@@ -54,15 +64,129 @@ class Instruction(object):
             outputstr += "\t"+self.arg2
         if self.arg3 is not None:
             outputstr += "\t"+self.arg3
+        return outputstr
 
 
 class TMachineCodeGenerator(object):
-    def __init__(self):
+    def __init__(self, symtable):
         self.instructions = []
+        self.table = symtable
+        self.registercnt = 0
 
     def addInstruction(self, instruction):
         self.instructions.append(instruction)
 
-    def generateInstructions(self, rootnodeP):
+    def getNewRegister(self):
+        self.registercnt += 1
+        return self.registercnt - 1
+
+    def getMemRegionWithVaraibleID(self, id, scope):
+        # TODO
+        pass
+
+    # given rootNodeP should be program node
+    def generateInstructions(self, rootNodeP: Program):
+        handleProgramDeclist(rootNodeP.DecList)
+        # TODO
+
+    # Processes Program's declist and add to the global variables area
+    def handleProgramDeclist(self, p: DecList):
+        # TODO
+        pass
+
+    # Returns the register number where the value of expr is stored after computing.
+    def handleExpr(self, p: Expr):
+        if p.expr_type == "paren":
+            prev = self.handleExpr(p.operand1)
+            return prev
+        elif p.expr_type == "unop":
+            prev = self.handleExpr(p.operand1)
+            result_register = self.getNewRegister()
+            type = p.return_type()
+            if type == "int":
+                instr = Instruction(Instruction.SUB, 0, getFromRegister(prev), register(result_register))
+            else:
+                instr = Instruction(Instruction.FSUB, 0, getFromRegister(prev), register(result_register))
+            self.addInstruction(instr)
+            return result_register
+        elif p.expr_type == "binop":
+            op1 = self.handleExpr(p.operand1)
+            op2 = self.handleExpr(p.operand2)
+            result_register = self.getNewRegister()
+            type = p.return_type()
+            if p.operator == "+":
+                if type == "int":
+                    instr = Instruction(Instruction.ADD, getFromRegister(op1), getFromRegister(op2),
+                                        register(result_register))
+                else:  # float case
+                    instr = Instruction(Instruction.FADD, getFromRegister(op1), getFromRegister(op2),
+                                        register(result_register))
+            elif p.operator == "-":
+                if type == "int":
+                    instr = Instruction(Instruction.SUB, getFromRegister(op1), getFromRegister(op2),
+                                        register(result_register))
+                else:  # float case
+                    instr = Instruction(Instruction.FSUB, getFromRegister(op1), getFromRegister(op2),
+                                        register(result_register))
+            elif p.operator == "*":
+                if type == "int":
+                    instr = Instruction(Instruction.MUL, getFromRegister(op1), getFromRegister(op2),
+                                        register(result_register))
+                else:  # float case
+                    instr = Instruction(Instruction.FMUL, getFromRegister(op1), getFromRegister(op2),
+                                        register(result_register))
+            elif p.operator == "/":
+                if type == "int":
+                    instr = Instruction(Instruction.DIV, getFromRegister(op1), getFromRegister(op2),
+                                        register(result_register))
+                else:  # float case
+                    instr = Instruction(Instruction.FDIV, getFromRegister(op1), getFromRegister(op2),
+                                        register(result_register))
+            elif p.operator == "==":
+                # 0 is false. Otherwise true.
+                # TODO
+                pass
+            elif p.operator == "!=":
+                # 0 is false. Otherwise true. If they are different, the subtraction will be nonzero.
+                if type == "int":
+                    instr = Instruction(Instruction.SUB, getFromRegister(op1), getFromRegister(op2),
+                                        register(result_register))
+                else:
+                    instr = Instruction(Instruction.FSUB, getFromRegister(op1), getFromRegister(op2),
+                                        register(result_register))
+            elif p.operator == "<":
+                # TODO
+                pass
+            elif p.operator == ">":
+                # TODO
+                pass
+            elif p.operator == "<=":
+                # TODO
+                pass
+            elif p.operator == ">=":
+                # TODO
+                pass
+            self.addInstruction(instr)
+            return result_register
+
+    def handleSpecialFunctionCall(self, p: Call):
+        if p.id == "printf" and len(p.arglist) == 1:
+            variableAddr = self.getMemRegionWithVaraibleID() # TODO
+            instr = Instruction(Instruction.WRITE, variableAddr)
+            self.addInstruction(instr)
+        elif p.id == "scanf" and len(p.arglist) == 1:
+            variableAddr = self.getMemRegionWithVaraibleID()  # TODO
+            if type == "int":
+                instr = Instruction(Instruction.READI, variableAddr)
+            else:
+                instr = Instruction(Instruction.READF, variableAddr)
+            self.addInstruction(instr)
+
+
+
+
+
+
+
 
 
